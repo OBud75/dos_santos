2ec450ebe12e2f9e8dec4d60c2719f1b5dc587f0
PaalKia <PaalKia.u.joker.1940@gmail.com>
2024-12-10 18:53:15 +0100

    Scripts

---
+++ LangageCPP/passwords2/password.hpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+#include <regex> // for Password::is_valid
+
+class Password {
+    private:
+        std::string raw_value;
+        std::string encrypted_value;
+        void encrypt();
+        bool is_valid(const std::string &password) const;
+
+    public:
+        Password(const std::string &password, bool is_encrypted = false);
+        std::string str() const;
+
+        bool operator==(const std::string &str) const;
+        bool operator==(const Password &other) const;
+        friend std::ostream &operator<<(std::ostream &os, const Password &p);
+};
